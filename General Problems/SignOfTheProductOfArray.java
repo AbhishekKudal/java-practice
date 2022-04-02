f37df04130ac22b90a4315e42da6624a3ded882a
Abhishek Kudal <abhikudal@gmail.com>
2022-04-02 08:25:03 +0530

    SignOfTheProductOfArray.java

---
+++ General Problems/SignOfTheProductOfArray.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int arraySign(int[] nums) {
+        int product = 1;
+        for(int i = 0; i<= nums.length - 1; i++){
+            if(product * nums[i] > 0){
+                product = 1;
+            }else if(product * nums[i] < 0){
+                product = -1;
+            }else{
+                return 0;
+            }
+        }
+        return product;
+    }
+}
