fe97ebcd27d5e82aab58b63ff8fb935c65069472
Abhishek Kudal <abhikudal@gmail.com>
2022-04-04 08:25:03 +0530

    1822. Sign of the Product of an Array

--- General Problems/SignOfTheProductOfArray.java
+++ General Problems/SignOfTheProductOfArray.java
@@ -1,3 +1,9 @@
+/*
+1822. Sign of the Product of an Array
+
+https://leetcode.com/problems/sign-of-the-product-of-an-array/
+*/
+
 class SignOfTheProductOfArray {
     public int arraySign(int[] nums) {
         int product = 1;
