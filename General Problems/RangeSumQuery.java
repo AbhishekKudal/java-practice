93ec9968e80ae028bab630ebf708e2bfa28eb3f3
Abhishek Kudal <abhikudal@gmail.com>
2022-05-11 15:26:06 +0530

    303. Range Sum Query - Immutable

---
+++ General Problems/RangeSumQuery.java
@@ -0,0 +1,22 @@
+class NumArray {
+
+    int[] nums1;
+    public NumArray(int[] nums) {
+        this.nums1 = nums;
+    }
+    
+    public int sumRange(int left, int right) {
+        int sum = 0;
+        
+        for(int start = left; start <= right; start ++){
+            sum += nums1[start];
+        }
+        return sum;
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
