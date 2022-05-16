56a51e1fc28c373122f2e75b2f0dbffd6ea40d85
Abhishek Kudal <abhikudal@gmail.com>
2022-05-16 22:16:06 +0530

    496. Next Greater Element I

--- General Problems/NextGreaterElement.java
+++ General Problems/NextGreaterElement.java
@@ -3,3 +3,28 @@
 
 https://leetcode.com/problems/next-greater-element-i/
 */
+
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] ans = new int[nums1.length];
+        for(int i = 0; i < nums1.length; i++){
+            for(int j = 0; j < nums2.length; j++){
+                if(nums1[i] == nums2[j]){
+                    if(j == nums2.length - 1){
+                        ans[i] = -1;
+                    }else{
+                        for(int k = j+1; k < nums2.length; k++){
+                            if(nums2[j] < nums2[k]){
+                                ans[i] = nums2[k];
+                                break;
+                            }else{
+                                ans[i] = -1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
