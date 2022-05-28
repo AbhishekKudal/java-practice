0a216019018973b573c0aeb106ad74b6fdc521a1
Abhishek Kudal <abhikudal@gmail.com>
2022-05-28 22:26:06 +0530

    1672. Richest Customer Wealth

--- General Problems/RichestCustomerWealth.java
+++ General Problems/RichestCustomerWealth.java
@@ -3,3 +3,20 @@
 
 https://leetcode.com/problems/richest-customer-wealth/
 */
+
+class RichestCustomerWealth {
+    public int maximumWealth(int[][] accounts) {
+        int sum = 0;
+        int ans = 0;
+        for(int row = 0; row < accounts.length; row++){
+            for(int j = 0; j < accounts[0].length ; j++){
+                sum = sum + accounts[row][j];
+            }
+            if(ans < sum){
+                ans = sum;
+            }
+            sum = 0;
+        }
+        return ans;
+    }
+}
