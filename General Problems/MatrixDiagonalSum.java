418fb7fa07df2b4a8bc77df1ea3c251594774edb
Abhishek Kudal <abhikudal@gmail.com>
2022-05-31 22:26:06 +0530

    1572. Matrix Diagonal Sum

--- General Problems/MatrixDiagonalSum.java
+++ General Problems/MatrixDiagonalSum.java
@@ -3,3 +3,19 @@
 
 https://leetcode.com/problems/matrix-diagonal-sum/
 */
+
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int sum = 0;
+        for(int row = 0; row < mat.length; row++){
+            sum += mat[row][row];
+        }
+        for(int row = 0; row < mat.length; row++){
+            sum += mat[row][mat[0].length - 1 - row];
+        }
+        if(mat.length % 2 != 0){
+            return sum - mat[(mat.length-1) / 2][(mat.length-1) / 2];
+        }
+        return sum ;
+    }
+}
