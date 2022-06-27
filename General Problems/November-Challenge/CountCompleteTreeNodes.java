58e38dfc0a9a66837868485011ccdd26b6a2e714
Abhishek Kudal <abhikudal@gmail.com>
2022-06-27 15:26:06 +0530

    Count Complete Tree Nodes

--- General Problems/November-Challenge/CountCompleteTreeNodes.java
+++ General Problems/November-Challenge/CountCompleteTreeNodes.java
@@ -23,7 +23,7 @@ https://leetcode.com/problems/count-complete-tree-nodes/
  
 class CountCompleteTreeNodes {
     public int countNodes(TreeNode root) {
-        if(root==null){
+        if(root == null){
             return 0;
         }
         return 1 + countNodes(root.left) + countNodes(root.right);
