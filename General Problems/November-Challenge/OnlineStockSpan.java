d9d3fbd24424b5118e4c3ca00647b3a4cbb0025d
Abhishek Kudal <abhikudal@gmail.com>
2022-07-11 15:26:06 +0530

    Online Stock Span

--- General Problems/November-Challenge/OnlineStockSpan.java
+++ General Problems/November-Challenge/OnlineStockSpan.java
@@ -4,3 +4,24 @@
 https://leetcode.com/problems/online-stock-span/
 */
 
+class StockSpanner {
+    
+    Stack<int[]> st;
+    
+    public StockSpanner() {
+        st = new Stack<>();
+    }
+    
+    public int next(int price) {
+        
+        int span = 1;
+        
+        while(st.size() > 0 && price >= st.peek()[0]){
+            span += st.pop()[1];
+        }
+        
+        st.push(new int[]{price, span});
+        
+        return span;
+    }
+}
