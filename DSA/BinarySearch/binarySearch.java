8ea6a579919fc7d369c78af1fd7d6cc56a1aaa85
Abhishek Kudal <102067485+AbhishekKudal@users.noreply.github.com>
2022-08-19 23:21:22 +0530

    Update binarySearch.java

--- DSA/BinarySearch/binarySearch.java
+++ DSA/BinarySearch/binarySearch.java
@@ -1,4 +1,25 @@
-//Search a given element in an Sorted Ascending Array; if found return its index else return -1
+/*https://leetcode.com/problems/binary-search/
+
+**Search a given element in an Sorted Ascending Array; if found return its index else return -1
+
+Question: Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. 
+          If target exists, then return its index. Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+
+Example 2:
+
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1
+
+*/
 
 public class binarySearch {
     public static void main(String[] args) {
