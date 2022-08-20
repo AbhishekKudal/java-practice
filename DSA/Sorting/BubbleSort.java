44ec64f211fead86b8d665ae51a824a3ced0cf77
Abhishek Kudal <102067485+AbhishekKudal@users.noreply.github.com>
2022-08-21 01:21:53 +0530

    Bubble sort

--- DSA/Sorting/BubbleSort.java
+++ DSA/Sorting/BubbleSort.java
@@ -10,7 +10,7 @@ Space complexity    --> O(1)    ->Constant
 */
 
 
-public class plainBubbleSort {
+public class BubbleSort {
     public static void main(String[] args) {
         int arr[] = {10,5,4,6,2};
         sort(arr);
