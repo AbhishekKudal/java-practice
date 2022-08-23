ecf9b94fa01ac1ecad34e5f09b1ead9388750d77
Abhishek Kudal <102067485+AbhishekKudal@users.noreply.github.com>
2022-08-23 20:28:24 +0530

    Update perfectSquare.java

--- DSA/BinarySearch/perfectSquare.java
+++ DSA/BinarySearch/perfectSquare.java
@@ -1,3 +1,21 @@
+/* * https://leetcode.com/problems/valid-perfect-square/
+ * 367. Valid Perfect Square
+
+Given a positive integer num, write a function which returns True if num is a perfect square else False.
+
+Follow up: Do not use any built-in library function such as sqrt.
+
+Example 1:
+
+Input: num = 16
+Output: true
+
+Example 2:
+
+Input: num = 14
+Output: false
+
+ */
 public class perfectSquare {
     
     public static void main(String[] args) {
