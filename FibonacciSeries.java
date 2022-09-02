5a54eb0af57bedbc8ad15befa355c5cd15696af4
Revan Patil <108737061+revanpatil@users.noreply.github.com>
2022-09-02 16:34:58 +0530

    Update FibonacciSeries.java

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -5,12 +5,24 @@ public class FibonacciSeries {
       int n = 15;
       int answer = 0;
           System.out.println("Fibonacci Series till " + n + " terms:");
-      
       for(int i=0;i<n;i++){
          answer = firstTerm + secondTerm;
          System.out.print(answer+", ");
          secondTerm = firstTerm;
          firstTerm = answer;
       }
+        int ans = fibo(7);
+         System.out.println(ans);
+    }
+    
+    // another better approach
+    public static fibo(int n){
+        if(n<2){
+            return n;
+        }
+        return fibo(n-1) + fibo(n-2);
     }
 }
+
+
+    
