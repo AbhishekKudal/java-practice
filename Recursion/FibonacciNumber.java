eb5bd2a60383d8691bb2b5a3bf67b82a072a71b9
Abhishek Kudal <102067485+AbhishekKudal@users.noreply.github.com>
2022-09-08 23:06:13 +0530

    509. Fibonacci Number

--- Recursion/FibonacciNumber.java
+++ Recursion/FibonacciNumber.java
@@ -4,9 +4,24 @@
 https://leetcode.com/problems/fibonacci-number/
 */
 
-class FibonacciNumber {
-    public int fib(int n) {
-        if(n < 2)return n;
-        return fib(n-1) + fib(n-2);
+public class Fibonacci {
+    public static void main(String[] args) {
+        System.out.println(fibo(10));               //Classic recursion
+        System.out.println(fiboFormula(10));        //Using complete formula
+        System.out.println(fiboShortFormula(10));   //Using short formula
+    }
+
+    static int fibo(int n){
+        if(n < 2) return n;
+        return fibo(n-1) + fibo(n-2);
+    }
+
+    static double fiboFormula(int n){
+        return (1 / Math.sqrt(5)) * (Math.pow(((1 + Math.sqrt(5))/2),n) - Math.pow(((1 - Math.sqrt(5))/2),n));
+    }
+
+    static double fiboShortFormula(int n){
+        return (Math.pow(((1 + Math.sqrt(5))/2),n)) / Math.sqrt(5);
+
     }
 }
