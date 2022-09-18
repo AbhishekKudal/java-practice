63d3eb92c9d28602e8edac9298936ba3bca71187
Abhishek Kudal <102067485+AbhishekKudal@users.noreply.github.com>
2022-09-18 23:09:54 +0530

    Create SingleNumber.java

---
+++ Bitwise_Operators/SingleNumber.java
@@ -0,0 +1,9 @@
+class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+        for(int i = 0; i < nums.length; i++){
+            ans = ans ^ nums[i];
+        }
+        return ans;
+    }
+}
