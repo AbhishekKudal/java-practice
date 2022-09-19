26d6b5507bf838ce77ec1b4ac32c1077bef70e2f
Abhishek Kudal <102067485+AbhishekKudal@users.noreply.github.com>
2022-09-19 21:56:42 +0530

    66. Plus One

--- Array/PlusOne.java
+++ Array/PlusOne.java
@@ -6,7 +6,7 @@ class Solution {
     public int[] plusOne(int[] digits) {
         int n = digits.length;
         
-        for(int i = n - 1; i >= 0; i--){
+        for(int i = n - 1; i >= 0; i--) {
             if(digits[i] < 9){      //if last digit is less than nine increment that index and return the Array
                 digits[i] ++;
                 return digits;
