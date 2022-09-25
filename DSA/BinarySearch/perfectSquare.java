7cbfc7e1a6b6bc7434f14e313fb68aa68fd68d33
Abhishek Kudal <102067485+AbhishekKudal@users.noreply.github.com>
2022-09-25 22:28:04 +0530

    367. Valid Perfect Square

--- DSA/BinarySearch/perfectSquare.java
+++ DSA/BinarySearch/perfectSquare.java
@@ -45,3 +45,45 @@ public class perfectSquare {
         return false;
     }
 }
+
+//With precision
+public class SquareRootWithPrecision {
+    public static void main(String args[]) {
+        int number = 40;
+        int precision = 3;
+        System.out.printf("%3f",sqrt(number, precision));
+    }
+    
+    static double sqrt(int n, int p){
+        int start = 0;
+        int end = n;
+        double root = 0.0;
+        int mid = 0;
+        
+        while(start <= end){
+            mid = start + (end - start)/2;
+            
+            if(mid * mid == n){
+                return mid;
+            }
+            
+            if(mid * mid > n){
+                end = mid - 1;
+            }else{
+                start = mid + 1;
+            }
+        }
+        
+        System.out.println(end);
+        root = end;
+        double incr = 0.1;
+        for(int i = 0; i < p; i++){
+            while(root * root <= n) {
+                root += incr;
+            }
+            root -= incr;
+            incr /= 10;
+        }
+        return root;
+    }
+}
