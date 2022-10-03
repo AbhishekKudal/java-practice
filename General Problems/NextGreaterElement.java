f3f3edee148b0dd613fda50ad2e94b6bddc9073e
AKL8 <AKL8@DP5-AKL8-IND>
2022-10-03 21:56:08 +0530

    Refactoring

--- General Problems/NextGreaterElement.java
+++ General Problems/NextGreaterElement.java
@@ -4,7 +4,7 @@
 https://leetcode.com/problems/next-greater-element-i/
 */
 
-class Solution {
+class NextGreaterElement {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int[] ans = new int[nums1.length];
         for(int i = 0; i < nums1.length; i++){
