b8abeb6386d4b3d746f1e9621a209040e757fbac
Abhishek Kudal <102067485+AbhishekKudal@users.noreply.github.com>
2022-10-06 00:16:27 +0530

    58. Length of Last Word

---
+++ General Problems/LengthOfLastWord.java
@@ -0,0 +1,40 @@
+/*
+58. Length of Last Word
+
+https://leetcode.com/problems/length-of-last-word/
+*/
+
+
+class Solution1 {
+    public int lengthOfLastWord(String s) {
+        int count = 0;
+        for(int i = s.length() - 1 ; i >= 0; i--){
+            if(s.charAt(i) != ' '){
+                count++;        //found a valid char, increment the count
+            }else{
+                if(count > 0){
+                    return count;
+                }
+            }
+        }
+        return count;
+    }
+}
+
+class Solution2 {
+    public int lengthOfLastWord(String s) {
+        int count = 0;
+        for(int i = s.length() - 1; i >= 0; i--){
+            if(s.charAt(i) == ' '){
+                continue;
+            }
+            if(s.charAt(i) != ' '){
+                count++;
+            }
+            if(i == 0 || s.charAt(i) != ' ' && s.charAt(i-1) == ' '){
+                break;
+            }
+        }
+        return count;
+    }
+}
