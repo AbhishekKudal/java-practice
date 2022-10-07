a417447542eaf5919d74b8bcd068b8f4e646d421
Abhishek Kudal <102067485+AbhishekKudal@users.noreply.github.com>
2022-10-07 23:40:38 +0530

    Bubble Sort Using Recursion

---
+++ Recursion/BubbleSortUsingRecursion.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class BubbleSortUsingRecursion {
+    public static void main(String[] args) {
+        int[] arr = {2,5,3,4,1};
+        sort(arr, arr.length - 1, 0);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void sort(int[] arr, int r, int c){
+        if(r == 0) return;
+
+        if(c < r){
+            if(arr[c] > arr[c+1]){
+                swap(arr,c,c+1);                
+            }
+            sort(arr, r, c + 1);
+        }else {
+            sort(arr, r-1, 0);
+        }
+    }
+
+    private static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
