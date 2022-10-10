172f359f1132d872a537982b1de2179c2922a7af
Abhishek Kudal <102067485+AbhishekKudal@users.noreply.github.com>
2022-10-10 23:24:09 +0530

    Create MergeSortUsingRecursion.java

---
+++ Recursion/MergeSortUsingRecursion.java
@@ -0,0 +1,56 @@
+//Merge Sort Using copy of Array
+
+//912. Sort an Array
+
+//https://leetcode.com/problems/sort-an-array/
+
+import java.util.Arrays;
+
+class Solution {
+    public int[] sortArray(int[] nums) {
+        if(nums.length == 1){
+            return nums;
+        }
+        
+        int mid = nums.length / 2;
+        
+        int[] left = sortArray(Arrays.copyOfRange(nums, 0, mid));
+        int[] right = sortArray(Arrays.copyOfRange(nums, mid, nums.length));
+        
+        return merge(left, right);
+    }
+    
+    private static int[] merge(int[] first, int[] second){
+        int[] mix = new int[first.length + second.length];
+        
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        
+        while(i < first.length && j < second.length){
+            if(first[i] < second[j]){
+                mix[k] = first[i];
+                i++;
+            }else{
+                mix[k] = second[j];
+                j++;
+            }
+            
+            k++;
+        }
+        
+        while(i < first.length){
+            mix[k] = first[i];
+            i++;
+            k++;
+        }
+        
+        while(j < second.length){
+            mix[k] = second[j];
+            j++;
+            k++;
+        }
+        
+        return mix;
+    }
+}
