1d77689139a06c0352912b36e34aa25687e3280c
Abhishek Kudal <102067485+AbhishekKudal@users.noreply.github.com>
2022-10-15 23:53:22 +0530

    905. Sort Array By Parity

--- General Problems/SortArrayByParity.java
+++ General Problems/SortArrayByParity.java
@@ -5,7 +5,7 @@ https://leetcode.com/problems/sort-array-by-parity/
 */
 
 class SortArrayByParity {
-    public int[] sortArrayByParity(int[] nums) {
+    public int[] sortArrayByParity1(int[] nums) {
         int[] ans = new int[nums.length];
         int last = ans.length - 1;
         int first = 0;
@@ -19,5 +19,26 @@ class SortArrayByParity {
             }
         }
         return ans;
+    }
+    
+    
+    //Constant space complexity with minimum Runtime
+    public int[] sortArrayByParity2(int[] nums) {
+        int i = 0;
+        int evenCounter = 0;
+
+        while(i < nums.length){
+
+            //wherever you find even number just swap it with evenCounter
+            if(nums[i] % 2 == 0){
+                int temp = nums[i];
+                nums[i] = nums[evenCounter];
+                nums[evenCounter] = temp;   
+                evenCounter++;
+            }   
+            i++;
+        }
+
+        return nums;
     }
 }
