04b7b232ce9f26f5801759ab22eda6cdd87e1c90
Abhishek Kudal <102067485+AbhishekKudal@users.noreply.github.com>
2022-10-17 15:29:12 +0530

    Remove a String and return the remaining part

---
+++ Recursion/Problems/RemoveAString.java
@@ -0,0 +1,17 @@
+public class RemoveAString {
+    public static void main(String[] args) {
+        System.out.println(skipApple("bccshapplejush"));
+    }
+
+    static String skipApple(String up){
+        if(up.isEmpty()){
+            return "";
+        }
+
+        if(up.startsWith("apple")){
+            return skipApple(up.substring(5));
+        }else{
+            return up.charAt(0) + skipApple(up.substring(1));
+        }
+    }
+}
