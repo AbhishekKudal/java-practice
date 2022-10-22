698a37ac67b301b202a751eab135800afef41512
Abhishek Kudal <102067485+AbhishekKudal@users.noreply.github.com>
2022-10-22 23:21:53 +0530

    78. Subsets
    https://leetcode.com/problems/subsets/

---
+++ DSA/SubSet/SubSets.java
@@ -0,0 +1,24 @@
+/*
+78. Subsets
+
+https://leetcode.com/problems/subsets/
+*/
+
+class SubSets {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> outer = new ArrayList<>();
+        
+        outer.add(new ArrayList<>());
+        
+        for(int num : nums){
+            int n = outer.size();
+            
+            for(int i = 0; i < n; i++){
+                List<Integer> internal = new ArrayList<>(outer.get(i));
+                internal.add(num);
+                outer.add(internal);
+            }
+        }
+        return outer;
+    }
+}
