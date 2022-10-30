6ad7cd4e72841a3206151488ad8498c53ba144a8
Abhishek Kudal <102067485+AbhishekKudal@users.noreply.github.com>
2022-10-30 23:58:15 +0530

    51. N-Queens

--- DSA/BackTracking/NQueensLeetCode.java
+++ DSA/BackTracking/NQueensLeetCode.java
@@ -6,34 +6,37 @@ https://leetcode.com/problems/n-queens/
 
 package BackTracking;
 
+import java.util.*;
+
 public class NQueensLeetCode {
     public static void main(String[] args) {
-        int n = 4;
-        boolean[][] board = new boolean[n][n];
-        System.out.println(queens(board, 0));
+        System.out.println(solveNQueens(5));
     }
 
-    private static int queens(boolean[][] board, int row){
-        if( row == board.length){
-            display(board);
-            System.out.println();
-            return 1;
+    public static List<List<String>> solveNQueens(int n) {
+        boolean board[][] = new boolean[n][n];
+        List<List<String>> ans = new ArrayList<>();
+        nQueens(board, 0, ans);
+        return ans;
+    }
+
+    public static void nQueens(boolean board[][], int row, List<List<String>> ans) {
+        if (row == board.length) {
+            ans.add(insert(board));
+            return;
         }
 
-        int count = 0;
-        
-        // placing the queen and checking for every row and col
         for (int col = 0; col < board.length; col++) {
             if(isSafe2(board, row, col)){
                 board[row][col] = true;
-                count = count + queens(board, row + 1);
+                nQueens(board, row + 1, ans);
                 board[row][col] = false;
             }
         }
 
-        return count;
+        // return;
     }
-    
+
     //Understanding simplified
     private static boolean isSafe2(boolean[][] board, int row, int col) {
 
@@ -71,25 +74,25 @@ public class NQueensLeetCode {
     //Kunal's code
     private static boolean isSafe(boolean[][] board, int row, int col) {
 
-        // check vertical row
+        //Checking straight upwords
         for (int i = 0; i < row; i++) {
-            if(board[i][col] == true){
+            if(board[i][col]){
                 return false;
             }
         }
 
-        //check left diagonal
-        int maxLeft = Math.min(row, col);
+        //Checking left diagonal
+        int maxLeft = Math.min(row,col);
         for (int i = 1; i <= maxLeft; i++) {
-            if(board[row - i][col - i]){
+            if(board[row - 1][col - 1]){
                 return false;
             }
         }
 
-        //check right diagonal
-        int maxRight = Math.min(row, board.length - col - 1);
+        //Checking right diagonal
+        int maxRight = Math.min(row,board.length - col - 1);
         for (int i = 1; i <= maxRight; i++) {
-            if(board[row - i][col + i]){
+            if(board[row - 1][col + 1]){
                 return false;
             }
         }
@@ -97,16 +100,21 @@ public class NQueensLeetCode {
         return true;
     }
 
-    private static void display(boolean[][] board) {
-        for(boolean[] row : board){
+    private static List<String> insert(boolean[][] board) {
+        List<String> ans = new ArrayList<>();
+
+        for (boolean[] row : board) {
+            StringBuilder eachRow = new StringBuilder();
             for (boolean element : row) {
-                if(element == true){
-                    System.out.print("Q ");
-                }else{
-                    System.out.print("X ");
+                if (element == true) {
+                    eachRow.append("Q");
+                } else {
+                    eachRow.append(".");
                 }
             }
-            System.out.println();
+            ans.add(eachRow.toString());
         }
+        return ans;
     }
+
 }
