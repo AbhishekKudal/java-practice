1b742d1d2d3c56a64cb196d8547911eb8496e0fc
Abhishek Kudal <102067485+AbhishekKudal@users.noreply.github.com>
2022-11-08 11:51:03 +0530

    Find Cycle Length of a Linked List

---
+++ General Problems/November-Challenge/CycleLength.java
@@ -0,0 +1,24 @@
+//Find out the length of the cycle if the head is given 
+
+public static int CycleLength(ListNode head){
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast){
+                ListNode temp = slow;
+                int length = 0;
+
+                do{
+                    temp = temp.next;
+                    length++;
+                }while(temp != slow);
+
+                return length;
+            }
+        }
+        return 0;
+    }
