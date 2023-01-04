06087d91e99548c6acfd93ad0dbfc8321d832e54
Abhishek Kudal <102067485+AbhishekKudal@users.noreply.github.com>
2023-01-04 21:20:34 +0530

    944. Delete Columns to Make Sorted
    https://leetcode.com/problems/delete-columns-to-make-sorted/

---
+++ General Problems/January2023/DeleteColumns.java
@@ -0,0 +1,23 @@
+/*
+944. Delete Columns to Make Sorted
+
+https://leetcode.com/problems/delete-columns-to-make-sorted/
+*/
+
+class Solution {
+    public int minDeletionSize(String[] strs) {
+
+        int count = 0;
+
+        for(int i = 0; i < strs[0].length(); i++){
+            for(int j = 0; j < strs.length - 1; j++){
+                if(strs[j].charAt(i) > strs[j+1].charAt(i)){
+                    count++;
+                    break;
+                }
+            }
+        }
+
+        return count;
+    }
+}
