5d9767590797d596d7c2ffde7f9d4e302b6785c8
Abhishek Kudal <102067485+AbhishekKudal@users.noreply.github.com>
2023-09-04 16:38:11 +0530

    Create average-of-levels-in-binary-tree.java

---
+++ DSA/Tree/average-of-levels-in-binary-tree.java
@@ -0,0 +1,54 @@
+//https://leetcode.com/problems/average-of-levels-in-binary-tree/description/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> result = new ArrayList<>();
+
+        if(root == null){
+            return result;
+        } 
+
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        queue.offer(root);
+
+        while(! queue.isEmpty()){
+            int levelSize = queue.size();
+            double currentTotal = 0;
+
+            for(int i=0; i < levelSize; i++){
+                TreeNode currentNode = queue.poll();
+                currentTotal += currentNode.val;
+
+                if(currentNode.left != null){
+                    queue.offer(currentNode.left);
+                }
+
+                if(currentNode.right != null){
+                    queue.offer(currentNode.right);
+                }
+            }
+
+            result.add(currentTotal/levelSize);
+        }
+
+        return result;
+    }
+}
