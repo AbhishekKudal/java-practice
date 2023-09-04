8f32c3a5ced29068dbb291523a2cf523b00441b3
Abhishek Kudal <102067485+AbhishekKudal@users.noreply.github.com>
2023-09-04 17:01:36 +0530

    Create findTheSuccessor.java

---
+++ DSA/Tree/findTheSuccessor.java
@@ -0,0 +1,28 @@
+//BFS - Find the successor
+
+public TreeNode findSuccessor(TreeNode root, int key){
+  if(root == null){
+    return null;
+  }
+
+  Queue<TreeNode> queue = new LinkedList<>();
+  queue.offer(root);
+  
+  while(queue.isEmpty()){
+    TreeNode currentNode = queue.poll();
+
+    if(currentNode.left != null){
+      queue.offer(currentNode.left);
+    }
+
+    if(currentNode.right != null){
+      queue.offer(currentNode.right);
+    }
+
+    if(currentNode.val == key){
+      break;
+    }
+  }
+
+  return queue.peek();
+}
